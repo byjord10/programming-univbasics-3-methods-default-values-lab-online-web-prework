9b9197aaa214c8bfb2da5200dacf25e34fc619e1
Brianna Jordan <bryvjordan9@gmail.com>
2019-07-21 23:22:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 def protein (protein = "meat")
   puts "meat"
 end
@@ -8,3 +9,7 @@ veg2 = "macaroni"
 meal_choice("broccoli", "macaroni")
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+
+def meal_choice (dialog)
+  puts "What a nutritious choice! #{meal_choice}"
+end 
