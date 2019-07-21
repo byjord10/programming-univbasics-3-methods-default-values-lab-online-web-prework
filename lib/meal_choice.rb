4aa69c96760f605f36586b42935120571892b991
Brianna Jordan <bryvjordan9@gmail.com>
2019-07-21 22:11:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,15 @@
 # Your code here
+require 'spec_helper'
+
+veg1 = "broccoli"
+veg2 = "macaroni"
+
+if protein == "meat"
+  put "meat"
+else protein == "tofu"
+  put "tofu"
+end
+
+meal_choice("broccoli", "macaroni")
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
